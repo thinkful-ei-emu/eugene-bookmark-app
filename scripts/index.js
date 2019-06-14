127431e324bb5cca54fbe3e8484a5dd4fd507b3b
geneggy <geneggy@gmail.com>
2019-06-14 11:56:30 -0700

    made some edits

--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,3 @@
 /* global Bookmark, store*/
 // eslint-disable-next-line no-unused-vars
 
-$(document).ready(function() {
-  Bookmark.bindEventListeners();
-  api.getItems()
-    .then((bookmarks) => {
-      bookmarks.forEach(bookmark => store.addBookmark(bookmark));
-      Bookmark.render();
-    });
-});
